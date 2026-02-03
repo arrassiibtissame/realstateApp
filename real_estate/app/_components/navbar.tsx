c65f8d04797eca982ec5121703752e33ed2ffe85
arrassiibtissame <ibtissame2.ar@gmail.com>
2026-02-03 23:15:01 +0200

    modify navbar

--- real_estate/app/_components/navbar.tsx
+++ real_estate/app/_components/navbar.tsx
@@ -1,57 +1,91 @@
-import React from "react";  
+"use client"
+import { Button } from '@/components/ui/button'
+import { SignInButton, UserButton, useUser } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
 function Navbar() {
 
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+    const {user} = useUser()
+
+
+
+  return (
+    <nav className="flex bg-black justify-between  items-center border mx-4 max-md:w-full max-md:justify-between border-slate-700 px-6 py-4 w-full !m-0 text-white text-lg">
     <a href="https://prebuiltui.com">
-        <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
+         <Image alt='logo' width={50} height={50} className="w-[100px] h-[100px] object-cover" src ="/logo.png"/>
     </a>
-
-    <button aria-label="Menu" id="menu-toggle" className="sm:hidden">
-        <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="21" height="1.5" rx=".75" fill="#426287"/>
-            <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287"/>
-            <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#426287"/>
-        </svg>
-    </button>
-
-    <div id="mobile-menu" className="hidden absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden">
-        <a href="#" className="block">Home</a>
-        <a href="#" className="block">About</a>
-        <a href="#" className="block">Contact</a>
-        <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
-            Login
-        </button>
+    <div className="hidden md:flex items-center  gap-6 ml-7">
+        <a href="#" className="relative overflow-hidden h-6 group">
+            <span className="block group-hover:-translate-y-full transition-transform duration-300">Products</span>
+            <span
+                className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Products</span>
+        </a>
+        <a href="#" className="relative overflow-hidden h-6 group">
+            <span className="block group-hover:-translate-y-full transition-transform duration-300">Stories</span>
+            <span
+                className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Stories</span>
+        </a>
+        <a href="#" className="relative overflow-hidden h-6 group">
+            <span className="block group-hover:-translate-y-full transition-transform duration-300">Pricing</span>
+            <span
+                className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Pricing</span>
+        </a>
+        <a href="#" className="relative overflow-hidden h-6 group">
+            <span className="block group-hover:-translate-y-full transition-transform duration-300">Docs</span>
+            <span
+                className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Docs</span>
+        </a>
     </div>
 
-    <div className="hidden sm:flex items-center gap-8">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+    <div className="hidden ml-14 md:flex items-center gap-4">
+        <button
+            className="border border-slate-600 hover:bg-slate-800 px-4 py-2 rounded-full text-sm font-medium transition">
+            Contact
+        </button>
 
-        <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-            <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </div>
+        {!user ? (
 
-        <div className="relative cursor-pointer">
-            <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full">3</button>
-        </div>
+            <SignInButton>
+                <Button>Get Started</Button>
+            </SignInButton>
 
-        <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
-            Login
+        ):(
+            <UserButton/>
+        )}
+       
+    </div>
+    <button id="menuToggle" className="md:hidden text-gray-600">
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
+            stroke-linecap="round" stroke-linejoin="round">
+            <path d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+    </button>
+    <div id="mobileMenu" className="absolute hidden top-48 text-base left-0 bg-black w-full flex-col items-center gap-4">
+        <a className="hover:text-indigo-600" href="#">
+            Products
+        </a>
+        <a className="hover:text-indigo-600" href="#">
+            Customer Stories
+        </a>
+        <a className="hover:text-indigo-600" href="#">
+            Pricing
+        </a>
+        <a className="hover:text-indigo-600" href="#">
+            Docs
+        </a>
+        <button
+            className="border border-slate-600 hover:bg-slate-800 px-4 py-2 rounded-full text-sm font-medium transition">
+            Contact
+        </button>
+        <button
+            className="bg-white hover:shadow-[0px_0px_30px_14px] shadow-[0px_0px_30px_7px] hover:shadow-white/50 shadow-white/50 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-slate-100 transition duration-300">
+            Get Started
         </button>
     </div>
 </nav>
 
+  )
+}
 
-    return (   
-        <div>Navbar</div>
-    );
-}   
-export default Navbar;
+export default Navbar
